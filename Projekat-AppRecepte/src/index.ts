04cda3de8f03a1f43cc07f4d780d7b7c36fe59ec
dulefb <101865768+dulefb@users.noreply.github.com>
2023-07-11 17:32:06 +0200

    added forms

--- Projekat-AppRecepte/src/index.ts
+++ Projekat-AppRecepte/src/index.ts
@@ -1,1 +1,20 @@
 import { getUser } from "./dbServices";
+import { drawSignup } from "./drawFunctions";
+
+document.querySelector("a[href='#kreiraj-nalog']").addEventListener("click",()=>{
+    let child = document.querySelector(".middle > div");
+    //console.log(child);
+    if(child!==null)
+        document.querySelector(".middle").removeChild(child);
+    drawSignup(document.querySelector(".middle"));
+});
+
+document.querySelector("a[href='#pocetna']").addEventListener("click",()=>{
+    let child = document.querySelectorAll(".middle > div");
+    //console.log(child);
+    if(child!==null){
+        child.forEach(x=>{
+            document.querySelector(".middle").removeChild(x);
+        });
+    }
+});
