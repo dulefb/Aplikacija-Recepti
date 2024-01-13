20b435e50dbcd7efb3fe927db49c7f7e6e71d7e8
dulefb <101865768+dulefb@users.noreply.github.com>
2024-01-13 21:19:18 +0100

    Update index.js

--- Server-side/src/index.js
+++ Server-side/src/index.js
@@ -11,15 +11,6 @@ const { portNumber } = require("../config/config");
 let activeUsers = [];
 let messagesList = [];
 
-function Crypt(text,key){
-
-}
-
-function Decrypt(text,key)
-{
-    
-}
-
 const server = http.createServer(async(req,res)=>{
     let path = url.parse(req.url,true);
     let queryData = path.query;
@@ -35,130 +26,23 @@ const server = http.createServer(async(req,res)=>{
 
     if(req.method.toLowerCase()==='options')
     {
-        if(rootPath[0]==='deleteUser')
-        {
-            if(queryData.username)
-            {
-                activeUsers = activeUsers.filter(x=>x.username!==queryData.username);
-                console.log("User: "+queryData.username+" deleted...");
-                res.writeHead(200,"OK",headers);
-                res.write("User deleted successfully");
-                res.end();
-            }
-            else
-            {
-                res.writeHead(400,"Error",headers);
-                res.write("User not deleted...");
-                res.end();
-            }
-        }
-        else if(rootPath[0]==='message')
-        {
-            if(queryData.text && queryData.text.length>0 && queryData.from && queryData.to)
-            {
-                res.writeHead(200,"OK",headers);
-                res.write("Message sent successfully");
-                res.end();
-            }
-            else
-            {
-                res.writeHead(404,"Error",headers);
-                res.write("File not found...");
-                res.end();
-            }
-        }
+        
     }
     if(req.method.toLowerCase()==='get')
     {
-        if(rootPath[0]==='activeUsers')
-        {
-            if(queryData.username && activeUsers.find(x=>x.username===queryData.username))
-            {
-                res.writeHead(200,"OK",headers);
-                res.write(JSON.stringify({
-                    activeUsers:activeUsers.filter(x=>x.username!==queryData.username),
-                    messageList:messagesList.filter(x=>x.to===queryData.username)
-                }));
-                messagesList = messagesList.filter(x=>x.to!==queryData.username);
-                res.end();
-            }
-            else{
-                res.writeHead(400,"Error",headers);
-                res.write("Valid username not provided.");
-                res.end();
-            }
-        }
+
     }
-    if(req.method.toLowerCase()==='post'){
-        if(rootPath[0]==="user")
-        {
-            if(queryData.username && queryData.age && queryData.city && queryData.address)
-            {
-                if(!activeUsers.find(x=>x.username===queryData.username))
-                {
-                    let user=new User();
-                    user.username=queryData.username;
-                    user.age=queryData.age;
-                    user.city=queryData.city;
-                    user.address=queryData.address;
-                    activeUsers.push(user);
-                    res.writeHead(200,"OK",headers);
-                    res.write("Username registry successful");
-                    res.end();
-                    console.log("User registered...\n");
-                    console.log(user);
-                }
-                else{
-                    res.writeHead(400,"Error",headers);
-                    res.write("User not registered. Username is not available.");
-                    res.end();
-                }
-            }
-            else{
-                res.writeHead(400,"Error",headers);
-                res.write("User not registered. Enter all data");
-                res.end();
-            }
-        }
+    if(req.method.toLowerCase()==='post')
+    {
+        
+    }
+    if(req.method.toLowerCase()==='delete')
+    {
+
     }
     if(req.method.toLowerCase()==='put')
     {
-        if(rootPath[0]==="message")
-        {
-            if(queryData.text && queryData.text.length>0 && queryData.from && queryData.to)
-            {
-                let msg = {
-                    text:queryData.text,
-                    crypted:queryData.crypted,
-                    from:queryData.from,
-                    to:queryData.to
-                };
-                if(activeUsers.find(x=>x.username===msg.to))
-                {
-                    messagesList.push(msg);
-                    res.writeHead(200,"OK",headers);
-                    res.write("Message sent successfully");
-                    res.end();
-                    console.log("Message sent...\n");
-                    console.log(msg);
-                }
-                else{
-                    res.writeHead(400,"Error",headers);
-                    res.write("Message not sent. User not available.");
-                    res.end();
-                }
-            }
-            else{
-                res.writeHead(400,"Error",headers);
-                res.write("Message not sent. Enter all data");
-                res.end();
-            }
-        }
-        else{
-            res.writeHead(404,"Error",headers);
-            res.write("File not found...");
-            res.end();
-        }
+        
     }
 });
 
