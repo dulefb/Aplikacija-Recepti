9e0b9bfd701c73de15a1ae85079f0177f49ebae7
dulefb <101865768+dulefb@users.noreply.github.com>
2024-01-17 02:48:28 +0100

    Update index.js

--- Server-side/src/index.js
+++ Server-side/src/index.js
@@ -6,8 +6,24 @@ const formidable = require("formidable");
 const { portNumber } = require("../config/config");
 //const redisClient = redis.createClient();
 
-let activeUsers = [];
-let messagesList = [];
+function processRequestBody(requset,callback){
+    let chunks = [];
+    requset.on("data", (chunk) => {
+        chunks.push(chunk);
+    });
+    requset.on("end", () => {
+        console.log("all parts/chunks have arrived");
+        const data = Buffer.concat(chunks);
+        console.log("Data: ", data);
+        const querystring = data.toString();
+        const parsedData = new URLSearchParams(querystring);
+        const dataObj = {};
+        for (var pair of parsedData.entries()) {
+          dataObj[pair[0]] = pair[1];
+        }
+        callback(dataObj);
+    });
+}
 
 const server = http.createServer(async(req,res)=>{
     let path = url.parse(req.url,true);
@@ -24,7 +40,10 @@ const server = http.createServer(async(req,res)=>{
 
     if(req.method.toLowerCase()==='options')
     {
-        
+        if(rootPath[0]==='checkBody'){
+            res.writeHead(200,'OK',headers);
+            res.end();
+        }
     }
     if(req.method.toLowerCase()==='get')
     {
@@ -32,7 +51,14 @@ const server = http.createServer(async(req,res)=>{
     }
     if(req.method.toLowerCase()==='post')
     {
-        
+        if(rootPath[0]==='checkBody'){
+            processRequestBody(req,(dataObj)=>{
+                console.log(dataObj);
+                res.writeHead(200,'OK',headers);
+                res.write(JSON.stringify(dataObj));
+                res.end();
+            });
+        }
     }
     if(req.method.toLowerCase()==='delete')
     {
